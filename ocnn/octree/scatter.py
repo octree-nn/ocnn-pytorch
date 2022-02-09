b76d575d227e8a472dfb854f36e8af938a116d66
wang-ps <wangps@hotmail.com>
2022-02-09 22:06:21 +0800

    Add scatter

---
+++ ocnn/octree/scatter.py
@@ -0,0 +1,33 @@
+import torch
+from typing import Optional
+
+
+def broadcast(src: torch.Tensor, other: torch.Tensor, dim: int):
+  if dim < 0:
+    dim = other.dim() + dim
+  if src.dim() == 1:
+    for _ in range(0, dim):
+      src = src.unsqueeze(0)
+  for _ in range(src.dim(), other.dim()):
+    src = src.unsqueeze(-1)
+  src = src.expand_as(other)
+  return src
+
+
+def scatter_add(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
+                out: Optional[torch.Tensor] = None,
+                dim_size: Optional[int] = None) -> torch.Tensor:
+  index = broadcast(index, src, dim)
+
+  if out is None:
+    size = list(src.size())
+    if dim_size is not None:
+      size[dim] = dim_size
+    elif index.numel() == 0:
+      size[dim] = 0
+    else:
+      size[dim] = int(index.max()) + 1
+    out = torch.zeros(size, dtype=src.dtype, device=src.device)
+    return out.scatter_add_(dim, index, src)
+  else:
+    return out.scatter_add_(dim, index, src)
