aa728c765caf2fbbed0be5dd6e4d13aa5007d79c
wang-ps <wangps@hotmail.com>
2022-02-19 22:01:10 +0800

    Add octree2vox

---
+++ ocnn/nn/octree2vox.py
@@ -0,0 +1,30 @@
+import torch
+
+from ocnn.octree import Octree, key2xyz
+from ocnn.nn import octree_depad
+
+
+def octree2voxel(data: torch.Tensor, octree: Octree, depth: int,
+                 nempty: bool = False):
+  r''' Converts the input feature to full-voxel-based representation.
+
+  Args:
+    data (torch.Tensor): The input feature.
+    octree (Octree): The corresponding octree.
+    depth (int): The depth of current octree.
+    nempty (bool): If True, :attr:`data` only contains the features of non-empty
+        octree nodes.  
+  '''
+
+  key = octree.keys[depth]
+  if nempty:
+    key = octree_depad(key, octree, depth)
+  x, y, z, b = key2xyz(key, depth)
+
+  num = 1 << depth
+  channel = data.shape[1]
+  batch_size = octree.batch_size
+  size = (batch_size, num, num, num, channel)
+  vox = torch.zeros(size, dtype=data.dtype, device=data.device)
+  vox[b, x, y, z] = data
+  return vox
