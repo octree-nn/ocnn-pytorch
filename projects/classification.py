99eadbf943dd3614de10b16ad6def9b8313e02d1
wang-ps <wangps@hotmail.com>
2022-02-22 18:29:06 +0800

    Add modelnet classification

---
+++ projects/classification.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn.functional as F
+import ocnn
+
+from solver import Solver, Dataset
+from datasets import get_modelnet40_dataset
+
+
+class ClsSolver(Solver):
+
+  def get_model(self, flags):
+    if flags.name.lower() == 'lenet':
+      model = ocnn.models.LeNet(flags.channel, flags.nout, stages=3)
+    # elif flags.name.lower() == 'resnet':
+    #   model = ocnn.ResNet(flags.depth, flags.channel, flags.nout,
+    #                       flags.resblock_num)
+    else:
+      raise ValueError
+    return model
+
+  def get_dataset(self, flags):
+    return get_modelnet40_dataset(flags)
+
+  def forward(self, batch):
+    octree, label = batch['octree'].cuda(), batch['label'].cuda()
+    logits = self.model(octree)
+    log_softmax = F.log_softmax(logits, dim=1)
+    loss = F.nll_loss(log_softmax, label)
+    pred = torch.argmax(logits, dim=1)
+    accu = pred.eq(label).float().mean()
+    return loss, accu
+
+  def train_step(self, batch):
+    loss, accu = self.forward(batch)
+    return {'train/loss': loss, 'train/accu': accu}
+
+  def test_step(self, batch):
+    with torch.no_grad():
+      loss, accu = self.forward(batch)
+    return {'test/loss': loss, 'test/accu': accu}
+
+
+if __name__ == "__main__":
+  ClsSolver.main()
---
+++ projects/datasets/__init__.py
@@ -0,0 +1,5 @@
+from .modelnet import get_modelnet40_dataset
+
+__all__ = [
+  'get_modelnet40_dataset'
+]
