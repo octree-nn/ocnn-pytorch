706479e0917e87a03de2f800ef08e986540b177d
wang-ps <wangps@hotmail.com>
2022-02-27 23:03:38 +0800

    Add upgrad_ckpt for segmentation

---
+++ projects/tools/upgrade_ckpt.py
@@ -0,0 +1,28 @@
+import sys
+import torch
+
+filename = sys.argv[1]
+weights = torch.load(filename, map_location='cpu')
+upgrade = dict()
+for key, val in weights.items():
+  print(key, val.shape)
+
+  if 'conv.weight' in key:
+    upgrade[key] = val.permute(2, 1, 0)
+
+  elif 'conv1x1.weight' in key:
+    if 'conv1x1.conv1x1' in key:
+      new_key = key.replace('conv1x1.conv1x1', 'conv.linear')
+    else:
+      new_key = key.replace('conv1x1', 'linear')
+    upgrade[new_key] = val.squeeze(2)
+
+  elif 'conv1x1.bias' in key:
+    new_key = key.replace('conv1x1.bias', 'linear.bias')
+    upgrade[new_key] = val
+
+  else:
+    upgrade[key] = val
+
+torch.save(upgrade, filename[:-4] + '.upgrade.pth')
+print('succ!')
