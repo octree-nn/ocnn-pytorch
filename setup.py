1828f4642866ae705098e1b91e54d45899cc3117
wang-ps <wangps@hotmail.com>
2022-03-12 10:26:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,26 @@ from setuptools import setup, find_packages
 
 __version__ = '2.0.0'
 
+with open("README.md", "r", encoding="utf-8") as fid:
+  long_description = fid.read()
+
 setup(
     name='ocnn',
     version=__version__,
     author='Peng-Shuai Wang',
     author_email='wangps@hotmail.com',
+    description='Octree-based Sparse Convolutional Neural Networks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/octree-nn/ocnn-pytorch',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=['torch', 'numpy'],
+    python_requires='>=3.6',
     zip_safe=False,
-    install_requires=["torch", "numpy"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
