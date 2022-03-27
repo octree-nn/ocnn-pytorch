47629a02ccfdf3ad4e081ec18db48c41584cab25
wang-ps <wangps@hotmail.com>
2022-03-27 21:36:29 +0800

    Test docker/bash.sh

--- docker/bash.sh
+++ docker/bash.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
 
+## Tested dockers:
+# pytorch/pytorch:1.9.0-cuda10.2-cudnn7-devel 
+# pytorch/pytorch:1.11.0-cuda11.3-cudnn8-devel
+
 ## Usage:
 
 # 1. Run the following command to enter the docker
 : '
   docker run --gpus=all --rm -it --ipc=host --net=host                        \
   -v /home/ps/workspace:/home/ps/workspace -v /mnt:/mnt                       \
-  --name=pytorch-1.9.0  pytorch/pytorch:1.9.0-cuda10.2-cudnn7-devel  
+  --name=pytorch-1.11.0  pytorch/pytorch:1.11.0-cuda11.3-cudnn8-devel  
 '
 
 # 2. Run the following command in the folder containing `ocnn-pytorch`
 : '
   cd /home/ps/workspace/docker
-  bash ocnn-pytorch/docker/docker.sh
+  bash ocnn2-pytorch/docker/bash.sh
 '
 
 # 3. Get the final output as follows:
 : '
-  ...................
+  ..................
   ----------------------------------------------------------------------
-  Ran 19 tests in 3.171s
+  Ran 18 tests in 4.709s
 
   OK
 '
 # 4. Commit and push the docker
 : '
   docker ps -a
-  docker commit <CONTAINER ID> wangps/ocnn:pytorch-1.9.0
-  docker push wangps/ocnn:pytorch-1.9.0 
+  docker commit <CONTAINER ID> wangps/ocnn:pytorch-1.11.0
+  docker push wangps/ocnn:pytorch-1.11.0
 '
 
 
 ## Start:
 
 echo Copy the code
-cp -r ocnn-pytorch /workspace/ocnn-pytorch
+cp -r ocnn2-pytorch /workspace/ocnn-pytorch
 
 echo Install tools
 apt-get update
@@ -49,6 +53,7 @@ cd /workspace/ocnn-pytorch
 python setup.py install
 
 echo Install packages
+cd /workspace/ocnn-pytorch/projects
 pip install -r requirements.txt
 
 echo Run testing
