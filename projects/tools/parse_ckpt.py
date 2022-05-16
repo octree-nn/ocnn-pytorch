6801c050ef352cf9f76813aa883afec0b8856fd8
wang-ps <wangps@hotmail.com>
2022-05-16 17:51:03 +0800

    Add parse_ckpt

---
+++ projects/tools/parse_ckpt.py
@@ -0,0 +1,42 @@
+import argparse
+import torch
+import numpy as np
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--ckpt', type=str, required=True,
+                    help='The path of the ckpt file')
+parser.add_argument('--skips', type=str, required=False, nargs='*',
+                    help="Skip specific variables")
+args = parser.parse_args()
+
+ckpt = args.ckpt
+skips = args.skips if args.skips else []
+
+
+def list_var():
+  # all_vars = torch.load(ckpt, map_location='cpu')['model']
+  all_vars = torch.load(ckpt, map_location='cpu')
+  total_num, counter = 0, 0
+  for name, var in all_vars.items():
+    shape = var.shape
+
+    exclude = False
+    for s in skips:
+      exclude = s in name
+      if exclude:
+        break
+    if exclude:
+      continue
+
+    shape_str = '; '.join([str(s) for s in shape])
+    shape_num = np.prod(shape)
+    print("{}, {}, [{}], {}".format(counter, name, shape_str, shape_num))
+    total_num += shape_num
+    counter += 1
+
+  print('Total parameters: {}'.format(total_num))
+
+
+if __name__ == '__main__':
+  list_var()
