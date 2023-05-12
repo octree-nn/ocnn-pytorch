958f8675639fcaa5bbd3846b1b8eb05f759265b5
wang-ps <wangps@hotmail.com>
2023-05-12 11:54:48 +0800

    Add test_group_norm

--- test/test_octree_norm.py
+++ test/test_octree_norm.py
@@ -13,7 +13,7 @@ import unittest
 from .utils import get_octree
 
 
-class OctreeNormTest(unittest.TestCase):
+class TestOctreeNorm(unittest.TestCase):
 
   def test_instance_norm(self):
     octree1 = get_octree(4)
@@ -50,6 +50,46 @@ class OctreeNormTest(unittest.TestCase):
 
       data = torch.cat([data1, data2], dim=2).squeeze(0).transpose(0, 1)
       octree_norm = ocnn.nn.OctreeInstanceNorm(channel, nempty)
+      out = octree_norm(data, octree, depth)
+
+      self.assertTrue(torch.allclose(out, out_gt, atol=5e-6))
+
+  def test_group_norm(self):
+    octree1 = get_octree(4)
+    octree2 = get_octree(5)
+    octree = ocnn.octree.merge_octrees([octree1, octree2])
+
+    # test 1 - full layer for depth 2
+    depth = 2
+    channel = 16
+    group = 4
+    nnum = octree1.nnum[depth]
+    data = torch.rand(2, channel, nnum)
+
+    group_norm = torch.nn.GroupNorm(num_channels=channel, num_groups=group)
+    out_gt = group_norm(data)
+
+    octree_norm = ocnn.nn.OctreeGroupNorm(channel, group, nempty=False)
+    data = data.permute(0, 2, 1).reshape(-1, channel)
+    out = octree_norm(data, octree, depth)
+    out = out.view(2, nnum, channel).permute(0, 2, 1)
+
+    self.assertTrue(torch.allclose(out, out_gt, atol=5e-6))
+
+    # test 2
+    for nempty in [True, False]:
+      depth = 4
+      nnum1 = octree1.nnum_nempty[depth] if nempty else octree1.nnum[depth]
+      data1 = torch.rand(1, channel, nnum1)
+      out_gt1 = group_norm(data1)
+
+      nnum2 = octree2.nnum_nempty[depth] if nempty else octree2.nnum[depth]
+      data2 = torch.rand(1, channel, nnum2)
+      out_gt2 = group_norm(data2)
+      out_gt = torch.cat([out_gt1, out_gt2], dim=2).squeeze(0).transpose(0, 1)
+
+      data = torch.cat([data1, data2], dim=2).squeeze(0).transpose(0, 1)
+      octree_norm = ocnn.nn.OctreeGroupNorm(channel, group, nempty)
       out = octree_norm(data, octree, depth)
 
       self.assertTrue(torch.allclose(out, out_gt, atol=5e-6))
