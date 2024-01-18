662be426b26209c3842fe41860ae40323de231d4
wang-ps <wangps@hotmail.com>
2024-01-18 22:41:34 +0800

    Version up

--- ocnn/__init__.py
+++ ocnn/__init__.py
@@ -12,7 +12,7 @@ from . import models
 from . import dataset
 from . import utils
 
-__version__ = '2.2.1'
+__version__ = '2.2.2'
 
 __all__ = [
     'octree',
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '2.2.1'
+__version__ = '2.2.2'
 
 with open("README.md", "r", encoding="utf-8") as fid:
   long_description = fid.read()
