55013cb50c02e1ad700f19aae25fd39784c06224
wang-ps <wangps@hotmail.com>
2026-01-31 19:55:02 +0800

    Update benckmark

--- test/benchmark_conv.py
+++ test/benchmark_conv.py
@@ -11,17 +11,17 @@ import spconv.pytorch as spconv
 
 
 device = 'cuda'
-depth2channel = {4: 512, 5: 256, 6: 128, 7: 128, 8: 64, 9: 32}
+depth2channel = {4: 512, 5: 256, 6: 128, 7: 128, 8: 64, 9: 32, 10: 32}
 fp32_precision = 'ieee' if not ocnn.nn.kernels.config.allow_tf32 else 'tf32'
 
 
 configs = [
     triton.testing.Benchmark(
         x_names=['depth'],
-        x_vals=[4, 5, 6, 7, 8, 9],
+        x_vals=[5, 6, 7, 8, 9, 10],
         line_arg='provider',
         line_vals=['torch', 'triton', 'spconv'],
-        line_names=['PyTorch', 'Triton', 'Spconv'],
+        line_names=['OctConv', 'OctConvT', 'Spconv'],
         styles=[('green', '-'), ('blue', '-'), ('yellow', '-')],
         ylabel='Latency (ms)',
         plot_name=f'{fp32_precision}-{mode}-{str(dtype)}',
@@ -36,86 +36,53 @@ configs = [
 def benchmark(depth, provider, mode, dtype):
   in_channel = depth2channel[depth]
   out_channel = in_channel
-  kernel_size = [3]
+  kernel_size = [3, 3, 3]
   stride = 1
   nempty = True
 
   if provider == 'spconv' and dtype == torch.bfloat16:
     return float('nan')
 
+  # Create modules
   if provider == 'torch':
-    model = (
-        ocnn.nn.OctreeConv(
-            in_channel,
-            out_channel,
-            kernel_size,
-            stride=stride,
-            nempty=nempty,
-            use_bias=True,
-        )
-        .type(dtype)
-        .to(device)
-    )
+    model = ocnn.nn.OctreeConv(
+        in_channel, out_channel, kernel_size, stride=stride,
+        nempty=nempty, use_bias=True,).type(dtype).to(device)
+
   elif provider == 'triton':
-    model = (
-        ocnn.nn.OctreeConvTriton(
-            in_channel,
-            out_channel,
-            kernel_size,
-            stride=stride,
-            nempty=nempty,
-            use_bias=True,
-        )
-        .type(dtype)
-        .to(device)
-    )
+    model = ocnn.nn.OctreeConvTriton(
+        in_channel, out_channel, kernel_size, stride=stride,
+        nempty=nempty, use_bias=True,).type(dtype).to(device)
   else:
-    model = (
-        spconv.SubMConv3d(
-            in_channel,
-            out_channel,
-            (3, 3, 3),
-            padding=1,
-            indice_key='test',
-            algo=spconv.ConvAlgo.MaskSplitImplicitGemm,
-            bias=True
-        )
-        .cuda()
-        .to(dtype)
-    )
+    model = spconv.SubMConv3d(
+        in_channel, out_channel, kernel_size, padding=1, indice_key='test',
+        algo=spconv.ConvAlgo.MaskSplitImplicitGemm, bias=True).cuda().to(dtype)
     model.weight.data.copy_(torch.randn(
-        out_channel, 3, 3, 3, in_channel, dtype=dtype, device=device))
+        out_channel, *kernel_size, in_channel, dtype=dtype, device=device))
     model.bias.data.copy_(torch.randn(out_channel, dtype=dtype, device=device))
 
+  # Generate coordinates and octree
   reso = 2**depth
-  coords = sphere_coords(2**depth)
+  pos = sphere_coords(2**depth)
+  pos = pos / reso * 2 - 1
+  octree = Octree(depth, 2, device=device)
+  octree.build_octree(Points(pos))
+  octree.construct_all_neigh()
+  nnum = octree.nnum_nempty[depth] if nempty else octree.nnum[depth]
+
+  # Generate input data
   if provider == 'spconv':
     feat = torch.randn(
-        len(coords),
-        in_channel,
-        device=device,
-        dtype=dtype,
-        requires_grad=True,
-    )
-    coords = torch.cat([torch.zeros(coords.shape[0], 1,
-                       device=device, dtype=torch.int32), coords], dim=-1)
+        nnum, in_channel, device=device, dtype=dtype, requires_grad=True,)
+    x, y, z, b = octree.xyzb(depth, nempty=nempty)
+    coords = torch.stack([b, x, y, z], dim=1).to(torch.int32)
     data = spconv.SparseConvTensor(
-        feat, coords, torch.Size([reso, reso, reso]), 1
-    )
+        feat, coords, torch.Size([reso, reso, reso]), 1)
     out_spconv = model(data)
     data.indice_dict = out_spconv.indice_dict.copy()
   else:
-    pos = coords / reso * 2 - 1
-    octree = Octree(depth, 2, device=device)
-    octree.build_octree(Points(pos))
-    octree.construct_all_neigh()
     data = torch.randn(
-        octree.nnum_nempty[depth],
-        in_channel,
-        device=device,
-        dtype=dtype,
-        requires_grad=True,
-    )
+        nnum, in_channel, device=device, dtype=dtype, requires_grad=True,)
 
   if mode == 'fwd':
     if provider != 'spconv':
